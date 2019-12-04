4b00d2485888e0b31f9fe3b28b764c39d6fd224c
Nyrox <root@nyrox.tech>
2019-12-04 21:49:38 +0100

    Some updates to the comp expr stuff

--- compiler/src/Program.fs
+++ compiler/src/Program.fs
@@ -3,6 +3,7 @@
 open NanopassTP.Generator
 open System
 open Parse
+open System.Reflection
 
 type ITerminal =
     abstract member Name: unit -> string
@@ -12,18 +13,12 @@ type ITerminal =
 
             
 type Language =
-    val Terminals: Type list
-    val Nonterminals: Type list
+    val Items: Type list
     val Entry: Type option
 
-    new() = { Terminals=[]; Nonterminals=[]; Entry=None; }
-    new(t, n, e) = { Terminals=t; Nonterminals=n; Entry=e }
+    new() = { Items=[]; Entry=None; }
+    new(i, e) = { Items=i; Entry=e }
 
-    member this.ParseTerminal<'a when 'a :> ITerminal> (): Parser<'a> =
-        let termType = Seq.find (fun t -> t = typeof<'a>) this.Terminals
-        fun input ->
-            (termType.GetMethod "ParseSelf").Invoke(null, [| input :> obj |]) :?> Option<'a> * string
-        
 let EmptyLanguage = Language()
 
 type LanguageBuilder() = 
@@ -33,51 +28,115 @@ type LanguageBuilder() =
     member self.Extends (pre: Language, lang: Language) =
         lang
 
-    [<CustomOperation("terminal")>]
-    member self.Terminal (lang: Language, term: Type) =
-        Language (term :: lang.Terminals, lang.Nonterminals, lang.Entry)
+    [<CustomOperation("item")>]
+    member self.Item<'a> (lang: Language, parser: unit -> 'a) =
+        Language (typeof<'a> :: lang.Items, lang.Entry)
 
-
-    // [<CustomOperation("nonterminal")>]
-    // member self.Nonterminal (lang: Language, term: ITerminal) =
-    //     { lang with
-    //         nonterminals = term :: lang.nonterminals
-    //     }
 
 let language = LanguageBuilder()
 
-type NumberLiteral = 
+type TransformCallback = obj * (obj -> obj) -> obj
+
+type Transform =
+    val from: Language option
+    val into: Language option
+    val transforms: (System.Type * TransformCallback) list
+
+    new() = { from=None; into=None; transforms=[] }
+    new(f, i, t) = {from=f; into=i; transforms=t}
+
+    member this._Transform t node =
+        t |> printfn "%A"
+        this.transforms
+            |> Seq.find (fun (p, _) -> p = t)
+            |> fun (p, map) ->
+                map (node, (fun o -> 
+                    this._Transform (o.GetType()) o
+                ))
+                
+    
+    member this.Transform<'a> (ast: 'a) =
+        this._Transform typeof<'a> ast
+
+type TransformBuilder() = 
+    member Self.Yield (()) = Transform ()
+
+    [<CustomOperation("from")>]
+    member self.From (prev: Transform, from: Language) =
+        Transform (Some from, prev.into, prev.transforms)
+
+    [<CustomOperation("into")>]
+    member self.Into (prev: Transform, into: Language) =
+        Transform (prev.from, Some into, prev.transforms)
+    
+    [<CustomOperation("transform")>]
+    member self.Transform (prev: Transform, map: ('a * (obj -> obj)) -> obj) =
+        let cb: TransformCallback = 
+            fun (a: obj, t) -> map (a :?> 'a, t)
+
+        Transform (prev.from, prev.into, (typeof<'a>, cb) :: prev.transforms)
+
+let pass = TransformBuilder()
+
+
+type BinaryOp =
+    | Add
+    | Subtract
+    | Multiply
+    | Divide
+
+
+
+type Value =
     val value: int32
 
     new(i) = {value=i}
 
-    static member ParseSelf (input: string) =
-        let parse = Parse.mapSome (NumberLiteral) Parse.number
-        parse input
-    
-    interface ITerminal with
-        member this.Name () = "Number"
-        member this.Parse = Parse.erase Parse.digit
-    
 
-let lang = language {
-    terminal typeof<NumberLiteral>
-    
-    // nonterminal "factor" (fun language input ->
-    //     language.ParseTerminal<IntegerLiteral>()
-    //     ()
-    // )
+type BinaryOperation = 
+    val op: BinaryOp
+    val left: Value
+    val right: Value
+
+    new(o, l, r) = {op=o;left=l;right=r}
+
+let mathlang = language {
+    item (fun x -> Value 5)
+    item (fun x -> BinaryOperation (Add, Value 5, Value 10))
 }
 
-// let lang2 = language {
-//     extends lang
-//     terminal (NumberLiteral ())
-// }
+let cast<'t> (a: obj) =
+    a :?> 't
 
-lang.ParseTerminal<NumberLiteral>() "123"
-    |> fst
-    |> (fun f -> f.Value.value)
+let codegen = pass {
+    from mathlang
+    
+    transform (fun (op: BinaryOperation, transform) ->
+        let l = (transform op.left |> cast<Value>).value
+        let r = (transform op.right |> cast<Value>).value
+
+        (match op.op with
+        | Add -> l + r
+        | Subtract -> l - r
+        | _ -> failwith "unimplemented") :> Object
+    )
+    
+    transform (fun (number: Value, transform) -> 
+        (Value (number.value)) :> obj
+    )
+}
+
+
+let ast = BinaryOperation (Add, Value 12, BinaryOperation (Sub, Value 12, Value 5))
+
+codegen.Transform (ast)
     |> printfn "%A"
+
+
+
+
+codegen |> printfn "%A"
+
 
 
 [<EntryPoint>]
