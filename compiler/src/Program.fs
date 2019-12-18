539eb0bf972533066654260303b7a43f0ed30c86
Nyrox <root@nyrox.tech>
2019-12-18 21:02:24 +0100

    Implements aids node handling

--- compiler/src/Program.fs
+++ compiler/src/Program.fs
@@ -13,7 +13,7 @@ type ITerminal =
 
             
 type Language =
-    val Items: Type list
+    val Items: (Type * Type option) list
     val Entry: Type option
 
     new() = { Items=[]; Entry=None; }
@@ -29,34 +29,47 @@ type LanguageBuilder() =
         lang
 
     [<CustomOperation("item")>]
-    member self.Item<'a> (lang: Language, parser: unit -> 'a) =
-        Language (typeof<'a> :: lang.Items, lang.Entry)
+    member self.Item<'a> (lang: Language, parser: unit -> 'a, childTypes: Type option) =
+        Language ((typeof<'a>, childTypes) :: lang.Items, lang.Entry)
 
 
 let language = LanguageBuilder()
 
-type TransformCallback = obj * (obj -> obj) -> obj
+// (node, children)
+type Node = {
+    data: obj;
+    dataType: Type;
+    children: Node list;
+}
+
+type TransformCallback = {
+    nodeType: Type;
+    childType: Type;
+    erasedCb: (Node -> Node) -> obj -> Node list -> Node;
+}
+
+
+
 
 type Transform =
     val from: Language option
     val into: Language option
-    val transforms: (System.Type * TransformCallback) list
+    val transforms: TransformCallback list
 
     new() = { from=None; into=None; transforms=[] }
     new(f, i, t) = {from=f; into=i; transforms=t}
 
-    member this._Transform t node =
-        t |> printfn "%A"
+    member this._Transform (node: Node) =
+        node.dataType |> printfn "%A"
         this.transforms
-            |> Seq.find (fun (p, _) -> p = t)
-            |> fun (p, map) ->
-                map (node, (fun o -> 
-                    this._Transform (o.GetType()) o
-                ))
+            |> Seq.find (fun transform -> transform.nodeType = node.dataType)
+            |> fun transform ->
+                transform.erasedCb this._Transform node.data node.children
+
                 
     
-    member this.Transform<'a> (ast: 'a) =
-        this._Transform typeof<'a> ast
+    member this.Transform (node: Node) =
+        this._Transform node
 
 type TransformBuilder() = 
     member Self.Yield (()) = Transform ()
@@ -70,11 +83,14 @@ type TransformBuilder() =
         Transform (prev.from, Some into, prev.transforms)
     
     [<CustomOperation("transform")>]
-    member self.Transform (prev: Transform, map: ('a * (obj -> obj)) -> obj) =
-        let cb: TransformCallback = 
-            fun (a: obj, t) -> map (a :?> 'a, t)
+    member self.Transform (prev: Transform, map: (Node -> Node) -> 'tNode -> 'tChildren -> Node) =
+        let cb = {
+            nodeType = typeof<'tNode>;
+            childType = typeof<'tChildren>;
+            erasedCb = fun transform node children -> map transform (node :?> 'tNode) (children :> obj :?> 'tChildren)
+        }
 
-        Transform (prev.from, prev.into, (typeof<'a>, cb) :: prev.transforms)
+        Transform (prev.from, prev.into, cb :: prev.transforms)
 
 let pass = TransformBuilder()
 
@@ -95,14 +111,12 @@ type Value =
 
 type BinaryOperation = 
     val op: BinaryOp
-    val left: Value
-    val right: Value
 
-    new(o, l, r) = {op=o;left=l;right=r}
+    new(o) = {op=o}
 
 let mathlang = language {
-    item (fun x -> Value 5)
-    item (fun x -> BinaryOperation (Add, Value 5, Value 10))
+    item (fun x -> Value 5) None
+    item (fun x -> BinaryOperation Add, [Value 5, Value 6]) (Some typeof<Value * Value>)
 }
 
 let cast<'t> (a: obj) =
@@ -111,25 +125,51 @@ let cast<'t> (a: obj) =
 let codegen = pass {
     from mathlang
     
-    transform (fun (op: BinaryOperation, transform) ->
-        let l = (transform op.left |> cast<Value>).value
-        let r = (transform op.right |> cast<Value>).value
+    transform (fun transform (op: BinaryOperation) (children: Node list) ->
+        let [left; right] = children
+        let l = transform left
+        let r = transform right
+        let lv = (l.data |> cast<Value>).value
+        let rv = (r.data |> cast<Value>).value
 
-        (match op.op with
-        | Add -> l + r
-        | Subtract -> l - r
-        | _ -> failwith "unimplemented") :> Object
+        let d = (match op.op with
+                | Add -> lv + rv
+                | Subtract -> lv - rv
+                | _ -> failwith "unimplemented") :> Object
+
+        {
+            data=d;
+            children=[];
+            dataType=typeof<int32>
+        }
     )
     
-    transform (fun (number: Value, transform) -> 
-        (Value (number.value)) :> obj
+    transform (fun transform (number: Value) _ -> 
+        {
+            data=Value (number.value);
+            children=[];
+            dataType=typeof<int32>
+        }
     )
 }
 
+let leaf (v: 'a): Node =
+    {data=v :> obj; children=[]; dataType= typeof<'a>}
 
-let ast = BinaryOperation (Add, Value 12, BinaryOperation (Sub, Value 12, Value 5))
+let node (v: 'a) (c: Node list): Node =
+    {
+        data=v;
+        children=c;
+        dataType=typeof<'a>
+    }
 
-codegen.Transform (ast)
+
+let ast = node (BinaryOperation Add) [
+    leaf <| Value 5;
+    leaf <| Value 7
+]
+
+codegen.Transform ast
     |> printfn "%A"
 
 
